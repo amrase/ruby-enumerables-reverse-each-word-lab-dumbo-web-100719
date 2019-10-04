6c22d74fba7c214f8dfabd32b10ac02b51032c34
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 04:17:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(array)
-words = array.split(" ")
-new_array = []
-new_array = words.collect{|word| word.reverse}
-new_array.join(" ")
+  words = array.split(" ")
+  new_array = []
+  new_array = words.collect{|word| word.reverse}
+  new_array.join(" ")
 
 end
